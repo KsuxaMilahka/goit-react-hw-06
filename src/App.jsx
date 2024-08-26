c585f49680bfe9348a452107e89800563c105c0d
KsuxaMilahka <ksuxamilahka35@gmail.com>
2024-08-26 02:04:17 +0200

    hw-06

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,38 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactForm from './components/ContactForm/ContactForm';
 
 import './App.css';
-
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  addContact,
+  deleteContact,
+  selectContacts,
+} from './redux/contactsSlise/contactsSlice';
+import {
+  selectNameFilter,
+  changeFilter,
+} from './redux/filtersSlise/filtersSlise';
 
 function App() {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('contacts')) || initialContacts;
-  });
-  const [filterValue, setFilterValue] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const filterValue = useSelector(selectNameFilter);
 
   const handleFilterChange = event => {
-    setFilterValue(event.target.value);
+    dispatch(changeFilter(event.target.value));
   };
 
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filterValue.toLowerCase()),
   );
 
-  // const handleClick = contactName => {
-  //   console.log('name: ', contactName);
-  // };
-
   const onAddContact = contact => {
-    const finalContact = {
-      ...contact,
-      id: nanoid(),
-    };
-
-    setContacts([finalContact, ...contacts]);
+    dispatch(addContact(contact));
   };
 
   const onDeleteContact = contactId => {
-    setContacts(contacts.filter(item => item.id !== contactId));
+    dispatch(deleteContact(contactId));
   };
 
   return (
